2e5ccdc0516d74569b8239d1dcaf350c67f58f51
Kaizen-246 <kc101@kent.ac.uk>
2026-02-08 15:22:09 +0000

    layout

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,4 +1,11 @@
-init_database()
+def init_database():
+    names = ["Picard", "Riker""", "Data", "Worf", "Leslie"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+    divs = ["Command", "Command", "Operations", "Security"]
+    ids= ["NCC-1701-A", "NCC-1701-B", "NCC-1701-C", "NCC-1701-D", "NCC-1701-E"]
+    return names, ranks, divs, ids
+
 display_menu()
 
 main()
+
